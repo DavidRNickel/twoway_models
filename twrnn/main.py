52391bf34a6070ca5eb531e8a69f7d383788af9e
DavidRNickel <dnickel@purdue.edu>
2025-05-22 02:50:39 -0400

    minor updates to rnn

--- twrnn/main.py
+++ twrnn/main.py
@@ -21,37 +21,47 @@ from make_argparser import make_parser
 if __name__=='__main__':
     # model setup
     parser, _ = make_parser()
-    parameter = parser.parse_args(sys.argv[1:])
-    use_cuda = parameter.use_cuda and torch.cuda.is_available()
+    conf = parser.parse_args(sys.argv[1:])
+    use_cuda = conf.use_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    parameter.device = device
+    conf.device = device
 
-    save_dir = parameter.save_dir 
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    os.makedirs(conf.save_dir, exist_ok=True) 
+    filenames = os.listdir(conf.save_dir)
+    p = re.compile(f'^{conf.log_file}*')
+    logfiles = sorted([s for s in filenames if p.match(s)])
+    if len(logfiles) == 0:
+        conf.log_file = conf.log_file + '.txt'
+    elif len(logfiles) == 1:
+        conf.log_file = conf.log_file.split('.')[0] + '_1.txt'
+    else:
+        lf = logfiles[-1].split('_')
+        lf_name, lf_ext = '_'.join(lf[:-1]), lf[-1]# split apart the 
+        n = int(lf_ext.split('.')[0])
+        conf.log_file = lf_name + f'_{int(n+1)}.txt'
     
     orig_stdout = sys.stdout
-    outfile = open(os.path.join(save_dir, parameter.train_log_file), 'w')
+    outfile = open(os.path.join(save_dir, conf.train_log_file), 'w')
     sys.stdout=outfile
 
-    if parameter.use_tensorboard:
+    if conf.use_tensorboard:
         from torch.utils.tensorboard import SummaryWriter
         writer = SummaryWriter(log_dir = os.path.join(save_dir, f'tblog_{datetime.now().strftime("False%Y%m%d-%H%M%S")}'))
 
     # Generate training data
-    SNR1 = parameter.SNR1               # SNR at User1 in dB
+    SNR1 = conf.SNR1               # SNR at User1 in dB
     np1 = 10**(-SNR1/10)   # noise power1 -- Assuming signal power is set to 1
     sigma1 = np.sqrt(np1)
-    SNR2 = parameter.SNR2               # SNR at User2 in dB
+    SNR2 = conf.SNR2               # SNR at User2 in dB
     np2 = 10**(-SNR2/10)
     sigma2 = np.sqrt(np2)
 
     # Training set: tuples of (stream, noise1, noise 2)
     N_train = int(1e7)  # number of training set
-    bit1_train     = torch.randint(0, 2, (N_train, parameter.N_bits, 1))
-    bit2_train     = torch.randint(0, 2, (N_train, parameter.N_bits, 1))
-    noise1_train   = sigma1*torch.randn((N_train, parameter.N_channel_use, 1)) 
-    noise2_train   = sigma2*torch.randn((N_train, parameter.N_channel_use, 1)) 
+    bit1_train     = torch.randint(0, 2, (N_train, conf.N_bits, 1))
+    bit2_train     = torch.randint(0, 2, (N_train, conf.N_bits, 1))
+    noise1_train   = sigma1*torch.randn((N_train, conf.N_channel_use, 1)) 
+    noise2_train   = sigma2*torch.randn((N_train, conf.N_channel_use, 1)) 
 
     # Validation
     N_validation = int(1e5)
@@ -60,36 +70,36 @@ if __name__=='__main__':
     print('np2: ', np2)
 
     if use_cuda:
-        model = Twoway_coding(parameter).to(device)
+        model = Twoway_coding(conf).to(device)
         torch.backends.cudnn.benchmark = True
     else:
-        model = Twoway_coding(parameter)
+        model = Twoway_coding(conf)
 
     model.sigma1 = sigma1
     model.sigma2 = sigma2
 
     print(model)
-    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=parameter.learning_rate)
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.confs()), lr=conf.learning_rate)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
     # Training
     num_epoch = 100
-    clipping_value = parameter.grad_clip
+    clipping_value = conf.grad_clip
     
     print('Before training ')
     print('weight_power1: ', model.weight_power1_normalized.cpu().detach().numpy().round(3))
     print('weight_power2: ', model.weight_power2_normalized.cpu().detach().numpy().round(3))
-    if parameter.attention_type==4:
+    if conf.attention_type==4:
         print('weight1_merge: ', model.weight1_merge_normalized.cpu().detach().numpy().round(3))
         print('weight2_merge: ', model.weight2_merge_normalized.cpu().detach().numpy().round(3))
-    if parameter.attention_type==5:
+    if conf.attention_type==5:
         print('weight1_merge_fwd: ', model.weight1_merge_normalized_fwd.cpu().detach().numpy().round(3))
         print('weight1_merge_bwd: ', model.weight1_merge_normalized_bwd.cpu().detach().numpy().round(3))
         print('weight2_merge_fwd: ', model.weight2_merge_normalized_fwd.cpu().detach().numpy().round(3))
         print('weight2_merge_bwd: ', model.weight2_merge_normalized_bwd.cpu().detach().numpy().round(3))
     print()
 
-    N_iter = (N_train//parameter.batch_size)
+    N_iter = (N_train//conf.batch_size)
     print(f'Device: {device}')
     print(f'N_iter: {N_iter}')
     nowtime_int = int(time.time())
@@ -97,8 +107,8 @@ if __name__=='__main__':
 
     epoch_start = 0
     ctr = 0
-    if parameter.loadfile is not None:
-        ckpt = torch.load(parameter.loadfile)
+    if conf.loadfile is not None:
+        ckpt = torch.load(conf.loadfile)
         model.load_state_dict(ckpt['model_state_dict'])
         optimizer.load_state_dict(ckpt['optimizer_state_dict'])
         scheduler.load_state_dict(ckpt['scheduler_state_dict'])
@@ -114,10 +124,10 @@ if __name__=='__main__':
         model.train() # model.training() becomes True
         loss_training = 0
         for i in range(N_iter):
-            bit1 = bit1_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
-            bit2 = bit2_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
-            noise1 = noise1_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_channel_use,1)
-            noise2 = noise2_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_channel_use,1)
+            bit1 = bit1_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(conf.batch_size, conf.N_bits,1) 
+            bit2 = bit2_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(conf.batch_size, conf.N_bits,1) 
+            noise1 = noise1_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(conf.batch_size, conf.N_channel_use,1)
+            noise2 = noise2_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(conf.batch_size, conf.N_channel_use,1)
 
             bit1   = bit1.to(device)
             bit2   = bit2.to(device)
@@ -129,17 +139,17 @@ if __name__=='__main__':
             X2_hat, X1_hat = model(bit1, bit2, noise1, noise2)
 
             # Define loss according to output type
-            if parameter.output_type == 'bit_vector':
+            if conf.output_type == 'bit_vector':
                 loss = F.binary_cross_entropy(X1_hat, bit1) + F.binary_cross_entropy(X2_hat, bit2)
-            elif parameter.output_type == 'one_hot_vector':
-                bit1_hot =  one_hot(bit1).view(parameter.batch_size, 2**parameter.N_bits, 1) # (batch,2^K,1)
-                bit2_hot =  one_hot(bit2).view(parameter.batch_size, 2**parameter.N_bits, 1)
+            elif conf.output_type == 'one_hot_vector':
+                bit1_hot =  one_hot(bit1).view(conf.batch_size, 2**conf.N_bits, 1) # (batch,2^K,1)
+                bit2_hot =  one_hot(bit2).view(conf.batch_size, 2**conf.N_bits, 1)
                 loss = (F.cross_entropy(X1_hat.squeeze(-1), torch.argmax(bit1_hot,dim=1).squeeze(-1).to(device)) # (batch,2^K), (batch)
                         + F.cross_entropy(X2_hat.squeeze(-1), torch.argmax(bit2_hot,dim=1).squeeze(-1).to(device)))
             
             # training
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+            torch.nn.utils.clip_grad_norm_(model.confs(), clipping_value)
             loss_training += loss.item()
             optimizer.step()
             
@@ -158,11 +168,11 @@ if __name__=='__main__':
                         'loss' : loss.item()},
                         os.path.join(save_dir, f'{nowtime}.pt'))
 
-        ber1_val, ber2_val, bler1_val, bler2_val, _, _ = test_model(model, parameter, N_validation)
+        ber1_val, ber2_val, bler1_val, bler2_val, _, _ = test_model(model, conf, N_validation)
         # Summary of each epoch
         print('Summary: Epoch: {}, lr: {}, Average loss: {:.4f}, BLER: {:.4f}'.format(epoch, optimizer.param_groups[0]['lr'], loss_training/N_iter, bler1_val+bler2_val) )
         eprint('Summary: Epoch: {}, lr: {}, Average loss: {:.4f}, BLER: {:.4f}'.format(epoch, optimizer.param_groups[0]['lr'], loss_training/N_iter, bler1_val+bler2_val) )
-        if parameter.use_tensorboard:
+        if conf.use_tensorboard:
             writer.add_scalar('BLER', bler1_val+bler2_val)
             writer.add_scalar('BLER1', bler1_val)
             writer.add_scalar('BLER2', bler2_val)
@@ -171,10 +181,10 @@ if __name__=='__main__':
         
         print('weight_power1: ', model.weight_power1_normalized.cpu().detach().numpy().round(3))
         print('weight_power2: ', model.weight_power2_normalized.cpu().detach().numpy().round(3))
-        if parameter.attention_type==4:
+        if conf.attention_type==4:
             print('weight1_merge: ', model.weight1_merge_normalized.cpu().detach().numpy().round(3))
             print('weight2_merge: ', model.weight2_merge_normalized.cpu().detach().numpy().round(3))
-        if parameter.attention_type==5:
+        if conf.attention_type==5:
             print('weight1_merge_fwd: ', model.weight1_merge_normalized_fwd.cpu().detach().numpy().round(3))
             print('weight1_merge_bwd: ', model.weight1_merge_normalized_bwd.cpu().detach().numpy().round(3))
             print('weight2_merge_fwd: ', model.weight2_merge_normalized_fwd.cpu().detach().numpy().round(3))
--- twrnn/testing.py
+++ twrnn/testing.py
@@ -1,8 +1,7 @@
 import torch
 import numpy as np
 
-import os
-import sys
+import os, sys, re
 from tqdm import tqdm
 
 from params import params
@@ -14,25 +13,36 @@ from make_argparser import make_parser
 if __name__=='__main__':
 
     parser, _ = make_parser()
-    parameter = parser.parse_args(sys.argv[1:])
-    use_cuda = parameter.use_cuda and torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
-    model = Twoway_coding(parameter).to(device)
-    ckpt = torch.load(parameter.loadfile)
+    conf = parser.parse_args(sys.argv[1:])
+    use_cuda = conf.use_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    conf.device = device
+    model = Twoway_coding(conf).to(device)
+    ckpt = torch.load(conf.loadfile)
     model.load_state_dict(ckpt['model_state_dict'])
 
-    save_dir = parameter.save_dir 
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
 
+    os.makedirs(conf.save_dir, exist_ok=True) 
+    filenames = os.listdir(conf.save_dir)
+    p = re.compile(f'^{conf.log_file}*')
+    logfiles = sorted([s for s in filenames if p.match(s)])
+    if len(logfiles) == 0:
+        conf.log_file = conf.log_file + '.txt'
+    elif len(logfiles) == 1:
+        conf.log_file = conf.log_file.split('.')[0] + '_1.txt'
+    else:
+        lf = logfiles[-1].split('_')
+        lf_name, lf_ext = '_'.join(lf[:-1]), lf[-1]# split apart the 
+        n = int(lf_ext.split('.')[0])
+        conf.log_file = lf_name + f'_{int(n+1)}.txt'
     orig_stdout = sys.stdout
-    outfile = open(os.path.join(save_dir, parameter.test_log_file), 'w')
+    outfile = open(os.path.join(conf.save_dir, conf.test_log_file), 'w')
     sys.stdout=outfile
 
-    SNR1 = parameter.SNR1               # SNR at User1 in dB
+    SNR1 = conf.SNR1               # SNR at User1 in dB
     np1 = 10**(-SNR1/10)   # noise power1 -- Assuming signal power is set to 1
     sigma1 = np.sqrt(np1)
-    SNR2 = parameter.SNR2               # SNR at User2 in dB
+    SNR2 = conf.SNR2               # SNR at User2 in dB
     np2 = 10**(-SNR2/10)
     sigma2 = np.sqrt(np2)
     model.sigma1 = sigma1
@@ -40,21 +50,21 @@ if __name__=='__main__':
 
     # N_train = int(1e7)  # number of training set
     N_train = int(1e5)
-    bit1_train     = torch.randint(0, 2, (N_train, parameter.N_bits, 1))
-    bit2_train     = torch.randint(0, 2, (N_train, parameter.N_bits, 1))
-    noise1_train   = sigma1*torch.randn((N_train, parameter.N_channel_use, 1)) 
-    noise2_train   = sigma2*torch.randn((N_train, parameter.N_channel_use, 1)) 
+    bit1_train     = torch.randint(0, 2, (N_train, conf.N_bits, 1))
+    bit2_train     = torch.randint(0, 2, (N_train, conf.N_bits, 1))
+    noise1_train   = sigma1*torch.randn((N_train, conf.N_channel_use, 1)) 
+    noise2_train   = sigma2*torch.randn((N_train, conf.N_channel_use, 1)) 
 
     # Validation
     N_validation = int(1e5)
 
     # Calculate mean/var with training data
     model.eval()   # model.training() becomes False
-    N_iter = N_train//parameter.batch_size
-    mean1_train = torch.zeros(parameter.N_channel_use)
-    std1_train  = torch.zeros(parameter.N_channel_use)
-    mean2_train = torch.zeros(parameter.N_channel_use)
-    std2_train  = torch.zeros(parameter.N_channel_use)
+    N_iter = N_train//conf.batch_size
+    mean1_train = torch.zeros(conf.N_channel_use)
+    std1_train  = torch.zeros(conf.N_channel_use)
+    mean2_train = torch.zeros(conf.N_channel_use)
+    std2_train  = torch.zeros(conf.N_channel_use)
     mean1_total = 0
     std1_total = 0
     mean2_total = 0
@@ -62,10 +72,10 @@ if __name__=='__main__':
 
     with torch.no_grad():
         for i in range(N_iter):
-            bit1   = bit1_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
-            bit2   = bit2_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
-            noise1 = noise1_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(bit1.shape[0], parameter.N_channel_use,1)
-            noise2 = noise2_train[parameter.batch_size*i:parameter.batch_size*(i+1),:,:].view(bit2.shape[0], parameter.N_channel_use,1)
+            bit1   = bit1_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
+            bit2   = bit2_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(parameter.batch_size, parameter.N_bits,1) 
+            noise1 = noise1_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(bit1.shape[0], parameter.N_channel_use,1)
+            noise2 = noise2_train[conf.batch_size*i:conf.batch_size*(i+1),:,:].view(bit2.shape[0], parameter.N_channel_use,1)
 
             bit1   = bit1.to(device)
             bit2   = bit2.to(device)
@@ -105,27 +115,27 @@ if __name__=='__main__':
     bler1_sum = 0
     ber2_sum  = 0
     bler2_sum = 0
-    power1_sum = np.zeros((parameter.batch_size, parameter.N_channel_use ,1))
-    power2_sum = np.zeros((parameter.batch_size, parameter.N_channel_use ,1))
+    power1_sum = np.zeros((conf.batch_size, conf.N_channel_use ,1))
+    power2_sum = np.zeros((conf.batch_size, conf.N_channel_use ,1))
 
     pbar = tqdm(total=N_iter)
     for ii in range(N_iter):
         pbar.update(1)
-        ber1_tmp, ber2_tmp, bler1_tmp, bler2_tmp, power1_tmp, power2_tmp = test_model(model, parameter, N_small)
+        ber1_tmp, ber2_tmp, bler1_tmp, bler2_tmp, power1_tmp, power2_tmp = test_model(model, conf, N_small)
         ber1_sum += ber1_tmp
         ber2_sum += ber2_tmp
         bler1_sum += bler1_tmp
         bler2_sum += bler2_tmp
         power1_sum += power1_tmp # (batch, N, 1)
         power2_sum += power2_tmp
-        if ii%100==0: 
-            print('Iter: {} out of {}'.format(ii, N_iter))
-            print('Ber1:  ', float(ber1_sum/(ii+1)))
-            print('Ber2:  ', float(ber2_sum/(ii+1)))
-            print('Bler1: ', float(bler1_sum/(ii+1)))
-            print('Bler2: ', float(bler2_sum/(ii+1)))
-            print('Power1: ', round(np.sum(power1_sum)/(parameter.batch_size*(ii+1)),3))
-            print('Power2: ', round(np.sum(power2_sum)/(parameter.batch_size*(ii+1)),3))
+        # if ii%100==0: 
+        #     print('Iter: {} out of {}'.format(ii, N_iter))
+        #     print('Ber1:  ', float(ber1_sum/(ii+1)))
+        #     print('Ber2:  ', float(ber2_sum/(ii+1)))
+        #     print('Bler1: ', float(bler1_sum/(ii+1)))
+        #     print('Bler2: ', float(bler2_sum/(ii+1)))
+        #     print('Power1: ', round(np.sum(power1_sum)/(conf.batch_size*(ii+1)),3))
+        #     print('Power2: ', round(np.sum(power2_sum)/(conf.batch_size*(ii+1)),3))
 
     ber1_inference  = ber1_sum/N_iter
     ber2_inference  = ber2_sum/N_iter
@@ -137,12 +147,12 @@ if __name__=='__main__':
     print('Ber2:  ', float(ber2_inference))
     print('Bler1: ', float(bler1_inference))
     print('Bler2: ', float(bler2_inference))
-    print('Power1: ', round(np.sum(power1_sum)/(parameter.batch_size*N_iter),3))
-    print('Power2: ', round(np.sum(power2_sum)/(parameter.batch_size*N_iter),3))
+    print('Power1: ', round(np.sum(power1_sum)/(conf.batch_size*N_iter),3))
+    print('Power2: ', round(np.sum(power2_sum)/(conf.batch_size*N_iter),3))
 
     ######## Save model
 
-    save_dir = parameter.save_dir
+    save_dir = conf.save_dir
     torch.save(model.state_dict(), os.path.join(save_dir,'final_state_dict.pt'))
 
     ####### Save normalization weights
